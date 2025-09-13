11e69b990f217ee7484fbbca151ce7645bf437a8
Levon Tehlerian <50045790+tehlerian@users.noreply.github.com>
2025-09-13 22:02:54 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/crypto-fundamental-dashboard/", // <-- смени с името на твоето GitHub repo
+  base: "/funda/", // <-- смени с името на твоето GitHub repo
 });
