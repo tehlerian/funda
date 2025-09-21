41565f5826e2d6b4fba7dbe0cbef5e9c26dc4df7
Levon Tehlerian <50045790+tehlerian@users.noreply.github.com>
2025-09-21 11:23:03 +0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/funda/", // <-- смени с името на твоето GitHub repo
-});
